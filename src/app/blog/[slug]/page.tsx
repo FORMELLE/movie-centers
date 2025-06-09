1c05ef3152d324f88cceeef494399cede19e16c2
FORMELLE <85746745+FORMELLE@users.noreply.github.com>
2025-06-09 19:09:31 +0200

    yellow everywere my friend

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,8 +17,8 @@ export async function generateStaticParams() {
 }
 
 export default async function BlogPage(props: BlogPageProps) {
-  const { params } = props
-  const { slug } = await params
+  const { params } = props;
+  const { slug } = await params;
   const post = await getPostBySlug(slug);
 
   if (!post) {
@@ -28,15 +28,24 @@ export default async function BlogPage(props: BlogPageProps) {
   const { frontMatter, contentHtml } = post;
 
   return (
-    <main className="container mx-auto p-5">
-    <Link href="/blog" style={{ color: 'blue'}} className="mb-3 underline">Back to blog</Link>
-    <article>
-      <h1>{frontMatter.title}</h1>
-      <time>{frontMatter.date}</time>
-      <hr />
+    <main className="container mx-auto p-5 text-[#f2cb07] bg-black min-h-screen">
+      <Link
+        href="/blog"
+        className="mb-3 inline-block underline text-[#f2cb07] hover:text-yellow-400"
+      >
+        Back to blog
+      </Link>
 
-      <div className="mt-4 markdown" dangerouslySetInnerHTML={{ __html: contentHtml }} />
-    </article>
+      <article>
+        <h1 className="text-3xl font-bold mb-2">{frontMatter.title}</h1>
+        <time className="text-sm text-[#f2cb07]">{frontMatter.date}</time>
+        <hr className="my-4 border-[#f2cb07]/40" />
+
+        <div
+          className="mt-4 markdown"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+      </article>
     </main>
   );
 }
